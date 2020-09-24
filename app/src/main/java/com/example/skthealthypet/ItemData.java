9fc2ce642f8f93eef770d003c78f87ec1f34d0b9
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-09-24 15:01:00 +0900

    운동 1차수정

---
+++ app/src/main/java/com/example/skthealthypet/ItemData.java
@@ -0,0 +1,10 @@
+package com.example.skthealthypet;
+
+public class ItemData {
+
+
+    public  ItemData(String title){
+        this.title=title;
+    }
+    public String title;
+}
--- app/src/main/java/com/example/skthealthypet/Pet_Fragment.java
+++ app/src/main/java/com/example/skthealthypet/Pet_Fragment.java
@@ -1,19 +1,17 @@
 package com.example.skthealthypet;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class Pet_Fragment extends Fragment {
-    Input_Activity activity= new Input_Activity();
-    String age = activity.getAge();
+
+
+
     public Pet_Fragment() {
         // Required empty public constructor
     }
@@ -26,7 +24,7 @@ public class Pet_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = LayoutInflater.from(getContext()).inflate(R.layout.fragment_pet_,null);
         TextView agetext=(TextView)v.findViewById(R.id.agetext);
-        agetext.setText(" "+age);
+        agetext.setText(" ");
         return v;
 
     }
