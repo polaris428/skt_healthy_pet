308559143fdec89404f481d1c31603a96712d2ff
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-09-25 06:18:35 +0900

    각종 업이데이트

--- app/src/main/java/com/example/skthealthypet/Recommended_Fragment.java
+++ app/src/main/java/com/example/skthealthypet/Recommended_Fragment.java
@@ -47,7 +47,8 @@ public class Recommended_Fragment extends Fragment {
     }
 
     //데이터 준비(최종적으로는 동적으로 추가하거나 삭제할 수 있어야 한다. 이 데이터를 어디에 저장할지 정해야 한다.)
-    private void setData() {
+    private void prepareData() {
+
         itemList.add(new item("농구"));
         itemList.add(new item("줄넘기"));
         itemList.add(new item("수영"));
