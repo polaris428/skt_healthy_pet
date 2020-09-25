98f7c78fc56f56ad6fe4e4cb9c5f73c17d788de5
jjhair119 <jmkjmk6666@gmail.com>
2020-09-26 03:57:17 +0900

    거의 최종

--- app/src/main/java/com/example/skthealthypet/Pedometer_Fragment.java
+++ app/src/main/java/com/example/skthealthypet/Pedometer_Fragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -23,7 +24,7 @@ import java.util.Date;
 
 
 public class Pedometer_Fragment extends Fragment  implements SensorEventListener {
-    Button mReset;
+    ImageButton mReset;
     TextView mwalknum;
     //현재 걸음 수
     private int mSteps = 0;
